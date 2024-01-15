e3e6dc423aede7ca247e5950f0f3b6d7725eb0e9
Raihan Nismara <raihannismara@gmail.com>
2024-01-15 11:42:42 +0700

    fix route api

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth.basic')->get('/', function() {
+    return response()->json('hello');
 });
+
+Route::resource('products', ProductController::class);
 
